ece8f0fac9d84dae18a99e31e8eeae3587628de0
OliEfr <ollihaus@t-online.de>
2025-05-13 16:23:10 +0200

    update .gitignore; committed missing file; set entropy reg to 0.01

---
+++ source/standalone/workflows/rsl_rl/actionManagerLatentActorMapping.py
@@ -0,0 +1,11 @@
+def get_vel_dependent_actor_latent_dim_for_action_manager_class(action_manager_class: str) -> int:
+    if action_manager_class == "LegwiseLatentActionManager":
+        return 9 # main freq, 4 legs with freq and amp each 1 + 4 * 2
+    elif action_manager_class == "InterpolatedStyleActionManager":
+        return 0 # no latent actions
+    elif action_manager_class == "FrequencyInterpolatedStyleActionManager":
+        return 1 # main frequency
+    elif action_manager_class == "ActionManager":
+        return 0 # Default
+    else:
+        raise ValueError(f"Unknown action_manager_class: {action_manager_class}")  
