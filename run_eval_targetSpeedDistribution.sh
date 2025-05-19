ed4c026112ddbdb2fc68905aed8560d3cff3d045
OliEfr <ollihaus@t-online.de>
2025-05-19 16:43:12 +0200

    fix eval bash script

--- run_eval_targetSpeedDistribution.sh
+++ run_eval_targetSpeedDistribution.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LC_NUMERIC=en_US.UTF-8 # required fix for correct float representation with "." and not ","
 
 seeds=(1 2)
 
@@ -17,15 +18,20 @@ echo "Sleeping for 5 seconds... Ctrl+C to abort"
 sleep 5
 
 for seed in "${seeds[@]}"; do
-    ./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py \
-        --amp_motion_folder 'datasets/manuallyGenerated/*' \
-        --task Isaac-Velocity-AMPFlat-Unitree-Go2-Play-v0 \
-        --load_run 2025-05-16_21-23-07_manuallyGenerated_SEED_${seed} \
-        --x_speed ${target_x_speeds[0]} \
-        --y_speed ${target_y_speeds[0]} \
-        --heading 0.0 \
-        --eval_config TargetXYDistribution \
-        --evaluate
+    for target_x_speed in "${target_x_speeds[@]}"; do
+        for target_y_speed in "${target_y_speeds[@]}"; do
+            echo "Target X Speed: ${target_x_speed}, Target Y Speed: ${target_y_speed}, Seed: ${seed}"
+            ./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py \
+                --amp_motion_folder 'datasets/manuallyGenerated/*' \
+                --task Isaac-Velocity-AMPFlat-Unitree-Go2-Play-v0 \
+                --load_run "2025-05-16_21-23-07_manuallyGenerated_SEED_${seed}" \
+                --x_speed=${target_x_speed} \
+                --y_speed=${target_y_speed} \
+                --heading=0.0 \
+                --eval_config TargetXYDistribution \
+                --evaluate
+        done
+    done
 done
 
 # Heading TARGET SPEEDS
@@ -40,16 +46,20 @@ echo "\n"
 total_experiments=$((${#seeds[@]} * ${#target_x_speeds[@]} * ${#target_headings[@]}))
 echo "Total number of experiments: ${#seeds[@]} * ${#target_x_speeds[@]} * ${#target_headings[@]} = $total_experiments"
 echo "Sleeping for 5 seconds... Ctrl+C to abort"
-sleep 5
+sleep 50
 
 for seed in "${seeds[@]}"; do
-    ./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py \
-        --amp_motion_folder 'datasets/manuallyGenerated/*' \
-        --task Isaac-Velocity-AMPFlat-Unitree-Go2-Play-v0 \
-        --load_run 2025-05-16_21-23-07_manuallyGenerated_SEED_${seed} \
-        --x_speed ${target_x_speeds[0]} \
-        --y_speed 0.0 \
-        --heading ${target_headings[0]} \
-        --eval_config TargetXHeadingDistribution \
-        --evaluate
+    for target_x_speed in "${target_x_speeds[@]}"; do
+        for target_heading in "${target_headings[@]}"; do
+            ./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py \
+                --amp_motion_folder 'datasets/manuallyGenerated/*' \
+                --task Isaac-Velocity-AMPFlat-Unitree-Go2-Play-v0 \
+                --load_run "2025-05-16_21-23-07_manuallyGenerated_SEED_${seed}" \
+                --x_speed=${target_x_speed} \
+                --y_speed=0.0 \
+                --heading=${target_heading} \
+                --eval_config TargetXHeadingDistribution \
+                --evaluate
+        done
+    done
 done
