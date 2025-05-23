1504ee54099804227e7482f5f04714b242541bef
OliEfr <ollihaus@t-online.de>
2025-05-23 21:06:36 +0200

    add delayed DC Motor actuator cfg, edit dockerignore, small fix

--- run_eval_targetSpeedDistribution.sh
+++ run_eval_targetSpeedDistribution.sh
@@ -1,65 +1,15 @@
 #!/bin/bash
 LC_NUMERIC=en_US.UTF-8 # required fix for correct float representation using "." instead of ","
 
-seeds=(1 2 3)
+# Repeat this because was broken on last run
+./targetXHeadingDistribution.sh "2025-05-16_21-23-07_manuallyGenerated_SEED_"
 
-# XY TARGET SPEEDS
-target_x_speeds=($(seq -1.0 0.1 1.0))
-target_y_speeds=($(seq -0.3 0.1 0.3))
+./targetXYDistribution.sh "2025-05-16_21-23-07_mocap_AMP_for_hardware_SEED_" # ~1500 exp
+./targetXHeadingDistribution.sh "2025-05-16_21-23-07_mocap_AMP_for_hardware_SEED_" #~500 exp
 
-echo "Executing experiments for: \n"
-echo "Target X Speeds: ${target_x_speeds[@]}"
-echo "Target Y Speeds: ${target_y_speeds[@]}"
-echo "Seeds: ${seeds[@]}"
-echo "\n"
-total_experiments=$((${#seeds[@]} * ${#target_x_speeds[@]} * ${#target_y_speeds[@]}))
-echo "Total number of experiments: ${#seeds[@]} * ${#target_x_speeds[@]} * ${#target_y_speeds[@]} = $total_experiments"
-echo "Sleeping for 5 seconds... Ctrl+C to abort"
-sleep 5
+./targetXYDistribution.sh "2025-05-16_21-23-07_fromVision_motions_DepthCam_SEED_"
+./targetXHeadingDistribution.sh "2025-05-16_21-23-07_fromVision_motions_DepthCam_SEED_"
 
-for seed in "${seeds[@]}"; do
-    for target_x_speed in "${target_x_speeds[@]}"; do
-        for target_y_speed in "${target_y_speeds[@]}"; do
-            echo "Target X Speed: ${target_x_speed}, Target Y Speed: ${target_y_speed}, Seed: ${seed}"
-            ./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py \
-                --task Isaac-Velocity-AMPFlat-Unitree-Go2-Play-v0 \
-                --load_run "2025-05-16_21-23-07_manuallyGenerated_SEED_${seed}" \
-                --x_speed=${target_x_speed} \
-                --y_speed=${target_y_speed} \
-                --heading=0.0 \
-                --eval_config TargetXYDistribution \
-                --evaluate
-            sleep 10 # prevent crashes
-        done
-    done
-done
+./targetXYDistribution.sh "2025-05-16_21-23-07_fromVision_motions_AlignedDepthAnything_SEED"
+./targetXHeadingDistribution.sh "2025-05-16_21-23-07_fromVision_motions_AlignedDepthAnything_SEED"
 
-# Heading TARGET SPEEDS
-target_x_speeds=($(seq -1.0 0.1 1.0))
-target_headings=($(seq -3.0 0.25 3.0))
-
-echo "Executing experiments for: \n"
-echo "Target X Speeds: ${target_x_speeds[@]}"
-echo "Target Headings: ${target_headings[@]}"
-echo "Seeds: ${seeds[@]}"
-echo "\n"
-total_experiments=$((${#seeds[@]} * ${#target_x_speeds[@]} * ${#target_headings[@]}))
-echo "Total number of experiments: ${#seeds[@]} * ${#target_x_speeds[@]} * ${#target_headings[@]} = $total_experiments"
-echo "Sleeping for 5 seconds... Ctrl+C to abort"
-sleep 50
-
-for seed in "${seeds[@]}"; do
-    for target_x_speed in "${target_x_speeds[@]}"; do
-        for target_heading in "${target_headings[@]}"; do
-            ./isaaclab.sh -p source/standalone/workflows/rsl_rl/play.py \
-                --task Isaac-Velocity-AMPFlat-Unitree-Go2-Play-v0 \
-                --load_run "2025-05-16_21-23-07_manuallyGenerated_SEED_${seed}" \
-                --x_speed=${target_x_speed} \
-                --y_speed=0.0 \
-                --heading=${target_heading} \
-                --eval_config TargetXHeadingDistribution \
-                --evaluate
-                sleep 10 # prevent crashes
-        done
-    done
-done
--- run_train.sh
+++ run_train.sh
@@ -3,7 +3,7 @@
 seeds=(3)
 
 current_datetime=$(date +"%Y-%m-%d_%H-%M-%S")
-
+ 
 for seed in "${seeds[@]}"; do
     ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py env.amp_motion_folder='datasets/manuallyGenerated/*' agent.amp_motion_folder='datasets/manuallyGenerated/*' --task Isaac-Velocity-AMPFlat-Unitree-Go2-v0 --headless --seed $seed --log_dir "${current_datetime}_manuallyGenerated"
 
@@ -18,3 +18,10 @@ for seed in "${seeds[@]}"; do
     ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-Flat-ComplexReward-Unitree-Go2-v0 --headless --seed $seed --log_dir "${current_datetime}_complexReward"
 
 done
+
+
+./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-Flat-ComplexReward-Unitree-Go2-v0 --headless --seed 1 --log_dir "testtest"
+
+Use this to get slow container: docker run --env-file .env.base --gpus all olivertum/isaac-lab-base:0.1
+
+Then start bash in container using docker exec -it container_id bash --> training will be slowdocker
