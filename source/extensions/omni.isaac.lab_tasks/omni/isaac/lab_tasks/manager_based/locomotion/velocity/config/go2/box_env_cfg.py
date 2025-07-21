4331e7c93f0dbbe0e22c96051f96b96144658c6f
OliEfr <ollihaus@t-online.de>
2025-07-21 14:22:43 +0200

    finalize box env parameters; remove residualRL envs

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/box_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/box_env_cfg.py
@@ -13,6 +13,7 @@ from omni.isaac.lab.managers import ObservationTermCfg as ObsTerm
 import omni.isaac.lab_tasks.manager_based.locomotion.velocity.mdp as mdp
 
 from . import parameters
+# from . import residual_rl_data
 
 #######################################################################
 # Box simple reward
@@ -26,12 +27,44 @@ class UnitreeGo2BoxEnvCfgSimpleReward(LocomotionVelocityRoughEnvCfg):
         
         self.terrain_type = "box"
         
+        parameters.disable_domain_randomization(self)
         parameters.set_terrain(self)
-        parameters.set_rewards_simple(self)
         parameters.set_box_env_cfg_reset_base(self)
         parameters.add_relative_position_to_box_observation(self)
         parameters.add_box_parameters_observation(self)
         parameters.set_box_env_cfg_cmds(self) # calling this last is the savest way
+        parameters.set_rewards_simple(self)
+        self.rewards.track_lin_vel_xy_exp.weight = 10.0 # need to increase to force agent to move
+        
+        self.scene.num_envs = 4096  # 5480
+        self.episode_length_s = 10.0
+
+        # RSI        
+        # rsi_params = {
+        #     "reference_states": ["joints", "base"],
+        # }
+        # parameters.set_amp_settings(self, motion_folder = "datasets/fromVision_motions_DepthCam_obstacle", **rsi_params)
+        
+        # self.is_amp_env = False
+        
+        
+        # ResidualRL
+        # self.action_manager_class = "ResidualRLActionManager"
+
+        # self.residual_rl_data = residual_rl_data.data[
+        #     "datasets/fromVision_motions_DepthCam_obstacle/obstacle_2_3126098000_amp.txt"
+        # ]
+        # self.actions.joint_pos.scale = 0.15
+
+        # self.observations.policy.phases = ObsTerm(func=mdp.phases, noise=None)
+
+        # self.scene.robot.actuators["base_legs"].stiffness = 50
+
+        # # we require the joint pos action offsets to be activated for ResRL
+        # assert (
+        #     self.actions.joint_pos.use_default_offset == True
+        # ), "Require default offset to be activated for residual RL."
+        
         
 
 
@@ -42,7 +75,7 @@ class UnitreeGo2BoxEnvCfgSimpleReward_PLAY(UnitreeGo2BoxEnvCfgSimpleReward):
         super().__post_init__()
 
         parameters.set_play_settings_flat(self)
-        parameters.set_play_settings_rough(self)
+        # self.events.reference_state_initialization = None
         
 #######################################################################
 # Box complex reward
@@ -56,7 +89,6 @@ class UnitreeGo2BoxEnvCfgComplexReward(UnitreeGo2BoxEnvCfgSimpleReward):
 
         parameters.set_rewards_complex(self)
         
-        self.rewards.feet_air_time = None # feet air time depends on base_velocity command
 
 @configclass
 class UnitreeGo2BoxEnvCfgComplexReward_PLAY(UnitreeGo2BoxEnvCfgComplexReward):
@@ -65,7 +97,7 @@ class UnitreeGo2BoxEnvCfgComplexReward_PLAY(UnitreeGo2BoxEnvCfgComplexReward):
         super().__post_init__()
         
         parameters.set_play_settings_flat(self)
-        parameters.set_play_settings_rough(self)
+        # self.events.reference_state_initialization = None
 
 #######################################################################
 # Box AMP
@@ -89,12 +121,9 @@ class AMPUnitreeGo2BoxEnvCfg(LocomotionVelocityRoughEnvCfg):
         parameters.set_velocity_rewards_amp(self)
 
         self.scene.num_envs = 5480  # w/o DR: 5480; w/ DR: 2 * 4096
-        
         self.episode_length_s = 10.0
 
-        # style
-        self.action_manager_class = "ActionManager"  # Default action manager
-
+        # RSI
         rsi_params = {
             "reference_states": ["joints", "base"],
         }
@@ -107,7 +136,6 @@ class AMPUnitreeGo2BoxEnvCfg(LocomotionVelocityRoughEnvCfg):
         assert (
             self.events.reference_state_initialization is not None
         ), "Always expecting RSI. For evaluation, please use the same motion files as used for training."
-        # self.events.reference_state_initialization.params["motion_files"] = motion_files
 
 
 
@@ -120,5 +148,8 @@ class AMPUnitreeGo2BoxEnvCfg_PLAY(AMPUnitreeGo2BoxEnvCfg):
         parameters.set_play_settings_flat(self)
         # parameters.set_play_settings_rough(self)
 
-        self.amp_motion_folder = "datasets/dummy/*"  # required otherwise it wont start; it is recomended to use same motion files as used for training
-
+        self.amp_motion_folder = "datasets/fromVision_motions_DepthCam_obstacle/*"  # required otherwise it wont start; it is recomended to use same motion files as used for training
+        
+        # self.events.reference_state_initialization = None
+        
+        
